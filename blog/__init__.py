1d6802103a8a4b91a7f32ffd5d3992ce0ab148dc
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2020-06-08 12:06:07 +0300

    Base and Error Pages

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,3 +2,11 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
+from blog.core.views import core
+from blog.error_pages.handlers import error_pages
+
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
